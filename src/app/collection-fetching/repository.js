30194d16ae9a1c66288adfa0f738b769c1780451
Radu B <radu.buciuceanu@gmail.com>
2020-08-11 20:50:46 +0300

    [chaos] Change collections repository.

--- src/app/collection-fetching/repository.js
+++ src/app/collection-fetching/repository.js
@@ -1,7 +1,20 @@
-import { fromFetch } from 'rxjs/fetch'
+import { from } from 'rxjs'
+import { map } from 'rxjs/operators'
+import { ApolloClient, InMemoryCache } from '@apollo/client'
+
+import allCollections from 'src/app/collection-fetching/all-collections.graphql'
 
 const URL = 'http://localhost:8080/graphql'
 
-export const fetchCollections = () => {
-  return null
-}
+const headers = () => ({
+  Authorization: 'Basic ' + btoa('admin:admin')
+})
+
+const client = () => new ApolloClient({
+  headers: headers(),
+  uri: URL,
+  cache: new InMemoryCache()
+})
+
+export const fetchCollections = () => from(client().query({ query: allCollections }))
+  .pipe(map(response => response.data))
