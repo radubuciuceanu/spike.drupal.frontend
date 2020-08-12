6728a63b9ed2c986033e25d2d8edff3430e0df44
Radu B <radu.buciuceanu@gmail.com>
2020-08-12 16:16:05 +0300

    [chaos] Change gatsby browser file.

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -7,8 +7,7 @@ import 'src/styles/global.css'
 import { rootReducer } from 'src/app/root-reducer'
 import { rootEpic } from 'src/app/root-epic'
 
-// eslint-disable-next-line react/display-name
-export default ({ element }) => {
+export const wrapRootElement = ({ element }) => {
   const epicMiddleware = createEpicMiddleware()
   const store = createStore(rootReducer, applyMiddleware(epicMiddleware))
 
