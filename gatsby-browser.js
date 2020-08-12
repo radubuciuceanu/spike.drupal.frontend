96d1878d0003f70436324059d24608fb437ce4fd
Radu B <radu.buciuceanu@gmail.com>
2020-08-12 15:59:31 +0300

    [chaos] Use redux.

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,12 @@
+import React from 'react'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+
 import 'src/styles/global.css'
+import { rootReducer } from 'src/app/root-reducer'
+
+// eslint-disable-next-line react/display-name
+export default ({ element }) => {
+  const store = createStore(rootReducer)
+  return <Provider store={store}>{element}</Provider>
+}
