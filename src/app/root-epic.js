afd92ce92fdf369d82c359f95b40e4484a54ae3c
Radu B <radu.buciuceanu@gmail.com>
2020-08-12 16:27:42 +0300

    [chaos] Register new epics.

--- src/app/root-epic.js
+++ src/app/root-epic.js
@@ -1,3 +1,5 @@
 import { combineEpics } from 'redux-observable'
 
-export const rootEpic = combineEpics()
+import { collection } from 'src/app/collection/epics'
+
+export const rootEpic = combineEpics(collection)
