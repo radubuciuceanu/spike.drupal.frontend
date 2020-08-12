c0c3697b05945d9a71d47eb994e9df12481cb0de
Radu B <radu.buciuceanu@gmail.com>
2020-08-12 18:47:43 +0300

    [chaos] Change collection components.

--- src/app/collection/inner.jsx
+++ src/app/collection/inner.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import GridListTile from '@material-ui/core/GridListTile'
 import GridList from '@material-ui/core/GridList'
+import Collapse from '@material-ui/core/Collapse'
 
 import { CollectionTitle } from 'src/app/collection/title'
 import { useSelector } from 'react-redux'
@@ -9,15 +10,16 @@ import { Item } from 'src/app/item'
 export const InnerCollection = ({ value }) => {
   const innerCollection = useSelector(root => root.collection.inner.toJS())
 
-  return <div hidden={innerCollection.title !== value.title}>
+  return <Collapse in={innerCollection.title === value.title}>
     <GridList cellHeight={20} style={{ overflow: 'hidden' }}>
       <GridListTile cols={3} style={{ height: 'auto' }}>
         <CollectionTitle value={innerCollection?.value ?? {}}/>
       </GridListTile>
 
       {
-        innerCollection?.entities?.map((reference, index) => <Item key={index} value={reference}/>)
+        innerCollection?.value?.queryFieldReferences?.entities
+          ?.map((reference, index) => <Item key={index} value={reference}/>)
       }
     </GridList>
-  </div>
+  </Collapse>
 }
