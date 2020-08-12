bf33d80b40e1289608661beeb1af23e4f7074a80
Radu B <radu.buciuceanu@gmail.com>
2020-08-12 18:03:27 +0300

    [chaos] Change collection reducer.

--- src/app/collection/reducers/inner.js
+++ src/app/collection/reducers/inner.js
@@ -2,16 +2,17 @@ import { fromJS } from 'immutable'
 import { display, hide } from 'src/app/collection/actions/types'
 
 const defaultState = fromJS({
-  value: null
+  value: null,
+  title: null
 })
 
 export const inner = (state = defaultState, action) => {
   if (action.type === display.innerCollection) {
-    return state.merge({ value: action.payload })
+    return state.merge({ value: action.payload.value, title: action.payload.title })
   }
 
   if (action.type === hide.innerCollection) {
-    return state.merge({ value: null })
+    return state.merge({ value: null, title: null })
   }
 
   return state
