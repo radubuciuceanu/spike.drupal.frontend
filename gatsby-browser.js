0489d41a3f715e2da0e5aa199ce2e2b83417c1c2
Radu B <radu.buciuceanu@gmail.com>
2020-08-12 16:10:59 +0300

    [chaos] Use redux observable.

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import { applyMiddleware, createStore } from 'redux'
+import { createEpicMiddleware } from 'redux-observable'
 
 import 'src/styles/global.css'
 import { rootReducer } from 'src/app/root-reducer'
+import { rootEpic } from 'src/app/root-epic'
 
 // eslint-disable-next-line react/display-name
 export default ({ element }) => {
-  const store = createStore(rootReducer)
+  const epicMiddleware = createEpicMiddleware()
+  const store = createStore(rootReducer, applyMiddleware(epicMiddleware))
+
+  epicMiddleware.run(rootEpic)
+
   return <Provider store={store}>{element}</Provider>
 }
