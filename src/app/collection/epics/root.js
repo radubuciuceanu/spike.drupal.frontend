69a67c9b7e3bbea412f0f5a59bb993e179f59d69
Radu B <radu.buciuceanu@gmail.com>
2020-08-12 17:15:35 +0300

    [chaos] Change collection epics.

--- src/app/collection/epics/root.js
+++ src/app/collection/epics/root.js
@@ -1,11 +1,11 @@
 import { ofType } from 'redux-observable'
 import { map, mergeMap } from 'rxjs/operators'
 
-import { ALL_COLLECTIONS } from 'src/app/collection/actions/types'
-import { allCollectionsFulfilled } from 'src/app/collection/actions'
+import { fetch } from 'src/app/collection/actions/types'
+import { rootCollectionsFulfilled } from 'src/app/collection/actions'
 import { rootCollections } from 'src/app/collection/repositories/repository'
 
 export const root = action$ => action$
-  .pipe(ofType(ALL_COLLECTIONS))
+  .pipe(ofType(fetch.rootCollections))
   .pipe(mergeMap(() => rootCollections()))
-  .pipe(map(result => allCollectionsFulfilled(result.nodeQuery)))
+  .pipe(map(result => rootCollectionsFulfilled(result.nodeQuery)))
