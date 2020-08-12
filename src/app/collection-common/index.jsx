a9a6617fc53b17a99f5d616a9b3125bf923ca107
Radu B <radu.buciuceanu@gmail.com>
2020-08-12 15:27:07 +0300

    [chaos] Change collection component.

--- src/app/collection-common/index.jsx
+++ src/app/collection-common/index.jsx
@@ -1,5 +1,24 @@
 import React from 'react'
+import Grid from '@material-ui/core/Grid'
+import GridList from '@material-ui/core/GridList'
+import GridListTile from '@material-ui/core/GridListTile'
+import ListSubheader from '@material-ui/core/ListSubheader'
+import Typography from '@material-ui/core/Typography'
 
-export const Collection = ({ value }) => {
-  return <div>{value.title}</div>
-}
+import { Reference } from 'src/app/reference-common'
+
+const CollectionTitle = ({ value }) => <GridListTile cols={3} style={{ height: 'auto' }}>
+  <ListSubheader component="div">
+    <Typography variant={'h6'}>{value.title}</Typography>
+  </ListSubheader>
+</GridListTile>
+
+export const Collection = ({ value }) => <Grid item xs={10} sm={10} md={10} lg={10}>
+  <GridList cellHeight={20} style={{ overflow: 'hidden' }}>
+    <CollectionTitle value={value}/>
+
+    {
+      value.queryFieldReferences.entities.map((reference, index) => <Reference key={index} value={reference}/>)
+    }
+  </GridList>
+</Grid>
