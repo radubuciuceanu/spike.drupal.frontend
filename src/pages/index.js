5e1b47578e119eaee5ba90367e812865977b2116
Radu B <radu.buciuceanu@gmail.com>
2020-08-12 13:39:06 +0300

    [chaos] Change home page.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { tap } from 'rxjs/operators'
 
-export default function Home() {
-  return <div>Hello world!</div>
+import { fetchCollections } from 'src/app/collection-fetching/repository'
+import { Collection } from 'src/app/collection-common'
+
+const useRootCollections = () => {
+  const [collections, setCollections] = useState([])
+
+  useEffect(() => {
+    fetchCollections()
+      .pipe(tap(response => setCollections(response.nodeQuery.entities)))
+      .subscribe()
+  }, [])
+
+  return collections
+}
+
+export default function Home () {
+  const rootCollections = useRootCollections()
+
+  return <div>
+    {
+      rootCollections.map(collection => <Collection value={collection}/>)
+    }
+  </div>
 }
