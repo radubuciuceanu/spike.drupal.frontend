d05e7c61ebe6069c16a6ec38549db3104b9f0c7d
Radu B <radu.buciuceanu@gmail.com>
2020-08-12 15:27:21 +0300

    [chaos] Change movie component.

--- src/app/movie-common/index.jsx
+++ src/app/movie-common/index.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
+import GridListTileBar from '@material-ui/core/GridListTileBar'
+import GridListTile from '@material-ui/core/GridListTile'
 
-export const Movie = ({ value }) => {
-  return <div>{value.title}</div>
-}
+export const Movie = ({ value }) => <GridListTile>
+  <img src={'https://material-ui.com/static/images/grid-list/camera.jpg'} alt={'Image...'} width={303}/>
+  <GridListTileBar title={value.title} subtitle={value.fieldShortDescription}/>
+</GridListTile>
