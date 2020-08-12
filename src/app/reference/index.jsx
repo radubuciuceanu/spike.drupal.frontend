a5cc67b5c4a0c3fcf7174148eee0a28659ff7c1a
Radu B <radu.buciuceanu@gmail.com>
2020-08-12 17:31:51 +0300

    [chaos] Change reference component.

--- src/app/reference/index.jsx
+++ src/app/reference/index.jsx
@@ -1,12 +1,8 @@
-import React, { Fragment, useState } from 'react'
+import React from 'react'
 import Box from '@material-ui/core/Box'
 import { makeStyles } from '@material-ui/styles'
-import Collapse from '@material-ui/core/Collapse'
-import Grid from '@material-ui/core/Grid'
 
 import { makeReference } from 'src/app/make-reference'
-import { Movie } from 'src/app/movie'
-import { Collection } from 'src/app/collection'
 
 const useReferenceStyles = makeStyles(theme => ({
   root: {
@@ -17,25 +13,11 @@ const useReferenceStyles = makeStyles(theme => ({
   }
 }))
 
-const ChildCollection = ({ value }) => {
-  const [is, setIs] = useState(true)
-
-  return <Fragment>
-    <Movie value={value} onClick={() => setIs(!is)}/>
-    <Collapse in={!is}>
-      <Grid item container>
-        <Collection value={value}/>
-      </Grid>
-    </Collapse>
-  </Fragment>
-}
-
 export const Reference = ({ value }) => {
   const classes = useReferenceStyles()
   const Component = makeReference(value)
 
   return <Box p={1} className={classes.root}>
     {Component ? <Component value={value}/> : null}
-    {value.entityBundle === 'collection' ? <ChildCollection value={value}/> : null}
   </Box>
 }
