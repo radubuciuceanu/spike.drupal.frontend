f28029e4dac6a66be9d623a6b5c2ca2630404c40
Radu B <radu.buciuceanu@gmail.com>
2020-08-12 16:31:20 +0300

    [chaos] Change collection reducer.

--- src/app/collection/reducers/all.js
+++ src/app/collection/reducers/all.js
@@ -1,7 +1,14 @@
-const defaultState = {
-  value: []
-}
+import { fromJS } from 'immutable'
+import { ALL_COLLECTIONS_FULFILLED } from 'src/app/collection/actions/types'
+
+const defaultState = fromJS({
+  value: {}
+})
 
 export const all = (state = defaultState, action) => {
+  if (action.type === ALL_COLLECTIONS_FULFILLED) {
+    return state.merge({ value: action.payload })
+  }
+
   return state
 }
